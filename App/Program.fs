f8b87268a4242bfd37ff11b67c45621fd16e63a4
ftnext <takuyafjp+develop@gmail.com>
2025-07-04 00:42:26 +0900

    [feat] Add logging
    https://learn.microsoft.com/ja-jp/aspnet/core/fundamentals/http-logging/?view=aspnetcore-8.0

--- App/Program.fs
+++ App/Program.fs
@@ -1,11 +1,15 @@
 open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.HttpLogging
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+    builder.Services.AddHttpLogging(fun options -> options.LoggingFields <- HttpLoggingFields.All) |> ignore
     let app = builder.Build()
+    app.UseHttpLogging() |> ignore
 
     app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
 
