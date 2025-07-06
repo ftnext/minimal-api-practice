a178d90cdec5e31f45dd56df313708d9ff97beb9
ftnext <takuyafjp+develop@gmail.com>
2025-07-06 15:47:03 +0900

    [feat] 1 Line, colorized log with timestamp

--- App/Program.fs
+++ App/Program.fs
@@ -1,10 +1,19 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Logging.Console
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+
+    builder.Logging.AddSimpleConsole(fun options ->
+        options.SingleLine <- true
+        options.TimestampFormat <- "HH:mm:ss "
+        options.ColorBehavior <- LoggerColorBehavior.Enabled
+    ) |> ignore
+
     let app = builder.Build()
 
     app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
